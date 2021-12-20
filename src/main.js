f6d84045f1030b83c2ccad6c7e035034431609be
xxuxuxuyang <1351267051@qq.com>
2021-12-20 23:03:59 +0800

    给src路径配置别名

--- src/main.js
+++ src/main.js
@@ -2,7 +2,8 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-import './css/base.css'
+// 初始化CSS 
+import '@/css/base.css'
 
 Vue.config.productionTip = false
 
